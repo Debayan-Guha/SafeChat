c218db9a6bec6ec176981e4555de170a7d62114d
Debayan-Guha <debayanguha03@gmail.com>
2026-04-17 12:21:47 +0530

    add eureka

---
+++ discoveryservice/src/test/java/com/safechat/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safechat.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
